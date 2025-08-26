9d73c865dca1592a0ecd6f7466c3f03a0adb061e
willymariino <wg.mariino@gmail.com>
2025-08-26 11:57:07 +0200

    add: useDate example

--- src/hooks/useDate.jsx
+++ src/hooks/useDate.jsx
@@ -8,6 +8,22 @@ Restituisce un oggetto con data e ora formattata.
 Si aggiorna automaticamente ogni secondo.
 Usa useEffect() per gestire l’aggiornamento.
 Esempio di utilizzo:
+
+import useDate from "./useDate";
+
+function App() {
+  const currentDate = useDate();
+
+  return (
+    <div>
+      <h1>Data e ora attuali:</h1>
+      <p>{currentDate.toLocaleString()}</p>
+    </div>
+  );
+}
+
+export default App;
+
 */
 
 import { useState, useEffect } from "react";
